4d1b9edfc08907a0e18890223f15a85048cea9a6
carlosyanezpuig <carlosyanez997@gmail.com>
2024-10-19 13:14:30 +0200

    Modify ExpenseRepository

--- composeApp/src/commonMain/kotlin/com/carlosdev/expensetracker/domain/repository/ExpenseRepository.kt
+++ composeApp/src/commonMain/kotlin/com/carlosdev/expensetracker/domain/repository/ExpenseRepository.kt
@@ -4,8 +4,8 @@ import com.carlosdev.expensetracker.domain.model.Expense
 import com.carlosdev.expensetracker.domain.model.ExpenseCategory
 
 interface ExpenseRepository {
-    suspend fun getExpenses(): List<Expense>
-    suspend fun addExpense(expense: Expense)
-    suspend fun editExpense(expense: Expense)
-    suspend fun getCategories(): List<ExpenseCategory>
+    fun getExpenses(): List<Expense>
+    fun addExpense(expense: Expense)
+    fun editExpense(expense: Expense)
+    fun getCategories(): List<ExpenseCategory>
 }

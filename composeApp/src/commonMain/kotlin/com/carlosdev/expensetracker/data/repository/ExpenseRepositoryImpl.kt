aa17fbc0b5e8a348fefb7d09c0def1393ee1409e
carlosyanezpuig <carlosyanez997@gmail.com>
2024-10-19 22:26:47 +0200

    Modify functions of ExpenseRepositoryImpl

--- composeApp/src/commonMain/kotlin/com/carlosdev/expensetracker/data/repository/ExpenseRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/com/carlosdev/expensetracker/data/repository/ExpenseRepositoryImpl.kt
@@ -5,20 +5,20 @@ import com.carlosdev.expensetracker.domain.model.Expense
 import com.carlosdev.expensetracker.domain.model.ExpenseCategory
 import com.carlosdev.expensetracker.domain.repository.ExpenseRepository
 
-class ExpenseRepositoryImpl(private val expenseManager: ExpenseManager): ExpenseRepository {
-    override suspend fun getExpenses(): List<Expense> {
+class ExpenseRepositoryImpl(private val expenseManager: ExpenseManager) : ExpenseRepository {
+    override fun getExpenses(): List<Expense> {
         return expenseManager.fakeExpenseList
     }
 
-    override suspend fun addExpense(expense: Expense) {
+    override fun addExpense(expense: Expense) {
         expenseManager.addExpense(expense)
     }
 
-    override suspend fun editExpense(expense: Expense) {
+    override fun editExpense(expense: Expense) {
         expenseManager.editExpense(expense)
     }
 
-    override suspend fun getCategories(): List<ExpenseCategory> {
+    override fun getCategories(): List<ExpenseCategory> {
         return expenseManager.getCategories()
     }
 }

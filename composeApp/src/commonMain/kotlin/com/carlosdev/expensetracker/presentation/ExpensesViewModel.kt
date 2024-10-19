35ed962cc612fa494a71290aa47dbe1a6d94f17e
carlosyanezpuig <carlosyanez997@gmail.com>
2024-10-19 13:30:25 +0200

    Add methods to ExpensesViewModel

--- composeApp/src/commonMain/kotlin/com/carlosdev/expensetracker/presentation/ExpensesViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/carlosdev/expensetracker/presentation/ExpensesViewModel.kt
@@ -5,7 +5,10 @@ import com.carlosdev.expensetracker.domain.repository.ExpenseRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import moe.tlaster.precompose.viewmodel.ViewModel
+import moe.tlaster.precompose.viewmodel.viewModelScope
 
 data class ExpensesUiState(
     val expenses: List<Expense> = emptyList(),
@@ -17,7 +20,41 @@ class ExpensesViewModel(private val expenseRepository: ExpenseRepository) : View
 
     private val _uiState = MutableStateFlow(ExpensesUiState())
     val uiState: StateFlow<ExpensesUiState> = _uiState.asStateFlow()
-    val allExpenses = expenseRepository.getExpenses()
+    private val allExpenses = expenseRepository.getExpenses()
 
+    init {
+        getAllExpenses()
+    }
 
+    private fun getAllExpenses() {
+        viewModelScope.launch {
+            updateState()
+        }
+    }
+
+    fun addExpense(expense: Expense) {
+        viewModelScope.launch {
+            expenseRepository.addExpense(expense)
+            updateState()
+        }
+
+    }
+
+    fun editExpense(expense: Expense) {
+        viewModelScope.launch {
+            expenseRepository.editExpense(expense)
+            updateState()
+        }
+
+    }
+
+    fun getExpensesById(id: Long): Expense {
+        return allExpenses.first{ it.id == id }
+    }
+
+    private fun updateState() {
+        _uiState.update { state ->
+            state.copy(expenses = allExpenses, total = allExpenses.sumOf { it.amount })
+        }
+    }
 }

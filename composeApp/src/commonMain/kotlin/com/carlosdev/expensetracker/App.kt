50968d3e113796f35b6f3971f197e1c2b24c3b94
carlosyanezpuig <carlosyanez997@gmail.com>
2024-10-19 22:27:42 +0200

    Add ExpensesScreen to App

--- composeApp/src/commonMain/kotlin/com/carlosdev/expensetracker/App.kt
+++ composeApp/src/commonMain/kotlin/com/carlosdev/expensetracker/App.kt
@@ -1,17 +1,23 @@
 package com.carlosdev.expensetracker
 
 import androidx.compose.runtime.Composable
+import com.carlosdev.expensetracker.data.ExpenseManager
+import com.carlosdev.expensetracker.presentation.ExpensesUiState
 import com.carlosdev.expensetracker.ui.ExpensesScreen
 import moe.tlaster.precompose.PreComposeApp
 
 @Composable
 fun App() {
 
-    val colors = getColorsTheme()
-
     PreComposeApp {
         AppTheme {
-            ExpensesScreen()
+            ExpensesScreen(
+                uiState = ExpensesUiState(
+                    expenses = ExpenseManager.fakeExpenseList,
+                    total = ExpenseManager.fakeExpenseList.sumOf { it.amount }
+                ),
+                onExpenseClick = {}
+            )
         }
     }
 }

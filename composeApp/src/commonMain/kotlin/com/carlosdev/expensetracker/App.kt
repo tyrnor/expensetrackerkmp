74a1e914621967801b6d44673a7a7a3d2864019b
carlosyanezpuig <carlosyanez997@gmail.com>
2024-10-20 10:46:17 +0200

    Move viewmodel to Navigator

--- composeApp/src/commonMain/kotlin/com/carlosdev/expensetracker/App.kt
+++ composeApp/src/commonMain/kotlin/com/carlosdev/expensetracker/App.kt
@@ -1,27 +1,14 @@
 package com.carlosdev.expensetracker
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import com.carlosdev.expensetracker.data.ExpenseManager
-import com.carlosdev.expensetracker.data.repository.ExpenseRepositoryImpl
-import com.carlosdev.expensetracker.presentation.ExpensesViewModel
-import com.carlosdev.expensetracker.ui.ExpensesScreen
 import moe.tlaster.precompose.PreComposeApp
-import moe.tlaster.precompose.flow.collectAsStateWithLifecycle
 
 @Composable
 fun App() {
 
     PreComposeApp {
-
-        val viewModel = ExpensesViewModel(ExpenseRepositoryImpl(ExpenseManager))
-        val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-
         AppTheme {
-            ExpensesScreen(
-                uiState = uiState,
-                onExpenseClick = {}
-            )
+
         }
     }
 }
